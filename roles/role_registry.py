05737e14f5c7dcd3f876bc56c63ca2357bd6e35a
asyahril-sha <asyahril.aiart@gmail.com>
2026-04-01 04:34:18 +0800

    Update role_registry.py

--- roles/role_registry.py
+++ roles/role_registry.py
@@ -1,6 +1,6 @@
 """Role registry untuk SERIVA.
 
-Mapping role_id -> instance Role (Nova, Siska, Dietha, Ipeh, Widya, dll.)
+Mapping role_id -> instance Role (Nova, Siska, Dietha, Ipeh, Widya, Aghnia, dll.)
 """
 
 from __future__ import annotations
@@ -13,6 +13,7 @@ from seriva.config.constants import (
     ROLE_ID_IPAR_TASHA,
     ROLE_ID_TEMAN_KANTOR_IPEH,
     ROLE_ID_TEMAN_LAMA_WIDYA,
+    ROLE_ID_TERAPIS_AGHIA,
 )
 from seriva.roles.base_role import Role
 from seriva.roles.nova import NovaRole
@@ -20,6 +21,7 @@ from seriva.roles.wanita_bersuami_siska import SiskaRole
 from seriva.roles.ipar_tasha import IparTashaRole
 from seriva.roles.teman_kantor_ipeh import TemanKantorIpehRole
 from seriva.roles.teman_lama_widya import TemanLamaWidyaRole
+from seriva.roles.terapis_aghia import TerapisAghiaRole
 
 
 # Inisialisasi instance role (singleton sederhana)
@@ -28,6 +30,7 @@ _siska_role = SiskaRole()
 _ipar_tasha_role = IparTashaRole()
 _ipeh_role = TemanKantorIpehRole()
 _widya_role = TemanLamaWidyaRole()
+_aghia_role = TerapisAghiaRole()
 
 
 ROLE_REGISTRY: Dict[str, Role] = {
@@ -36,7 +39,8 @@ ROLE_REGISTRY: Dict[str, Role] = {
     ROLE_ID_IPAR_TASHA: _ipar_tasha_role,
     ROLE_ID_TEMAN_KANTOR_IPEH: _ipeh_role,
     ROLE_ID_TEMAN_LAMA_WIDYA: _widya_role,
-    # Nanti tambahkan role lain di sini (terapis_aghia, terapis_munira, teman_spesial_*, dll.)
+    ROLE_ID_TERAPIS_AGHIA: _aghia_role,
+    # Nanti tambahkan role lain di sini (terapis_munira, teman_spesial_*, dll.)
 }
 
 
