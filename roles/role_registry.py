3d18d88b0af8ecc9a9e7ce50f27f2f347d08cec4
asyahril-sha <asyahril.aiart@gmail.com>
2026-04-01 04:23:27 +0800

    Update role_registry.py

--- roles/role_registry.py
+++ roles/role_registry.py
@@ -1,6 +1,6 @@
 """Role registry untuk SERIVA.
 
-Mapping role_id -> instance Role (Nova, Siska, dll.)
+Mapping role_id -> instance Role (Nova, Siska, Dietha, dll.)
 """
 
 from __future__ import annotations
@@ -10,21 +10,25 @@ from typing import Dict
 from seriva.config.constants import (
     ROLE_ID_NOVA,
     ROLE_ID_WANITA_BERSUAMI_SISKA,
+    ROLE_ID_IPAR_TASHA,
 )
 from seriva.roles.base_role import Role
 from seriva.roles.nova import NovaRole
 from seriva.roles.wanita_bersuami_siska import SiskaRole
+from seriva.roles.ipar_tasha import IparTashaRole
 
 
 # Inisialisasi instance role (singleton sederhana)
 _nova_role = NovaRole()
 _siska_role = SiskaRole()
+_ipar_tasha_role = IparTashaRole()
 
 
 ROLE_REGISTRY: Dict[str, Role] = {
     ROLE_ID_NOVA: _nova_role,
     ROLE_ID_WANITA_BERSUAMI_SISKA: _siska_role,
-    # Nanti tambahkan role lain di sini (ipar_tasha, teman_kantor_ipeh, dll.)
+    ROLE_ID_IPAR_TASHA: _ipar_tasha_role,
+    # Nanti tambahkan role lain di sini (teman_kantor_ipeh, teman_lama_widya, dll.)
 }
 
 
