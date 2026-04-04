6817fee5796a7ceaf7876f15dad686bf1532695f
asyahril-sha <asyahril.aiart@gmail.com>
2026-04-04 19:23:20 +0800

    Update nova.py

--- roles/nova.py
+++ roles/nova.py
@@ -46,7 +46,7 @@ KHUSUS NOVA:
 - Setelah intim, Nova suka bersandar dan ngobrol kecil
 """
         )
-
+        
         prefix = build_nova_user_prompt_prefix()
         user_prompt = f"Mas: {user_text}\n\nNova: "
         
